7c39914e75e4f952993b38e690b7d5fd58c159ca
Fathy2050 <mo.fathy2050r@gmail.com>
2023-08-06 13:27:49 +0300

    implementing google and facebook login

---
+++ lib/generated/codegen_loader.g.dart
@@ -0,0 +1,114 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>?> load(String path, Locale locale) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ar = {
+  "login": "تسجيل الدخول",
+  "login_sub_title": "يرجى تسجيل الدخول لاستكشاف منتجاتنا",
+  "register": "التسجيل",
+  "register_sub_title": "سجّل لاستكشاف منتجاتنا وآخر الأخبار",
+  "username": "اسم المستخدم",
+  "username_hint": "يرجى إدخال اسم المستخدم الخاص بك",
+  "email": "البريد الإلكتروني",
+  "email_hint": "يرجى إدخال البريد الإلكتروني الخاص بك",
+  "password": "كلمة المرور",
+  "password_hint": "يرجى إدخال كلمة المرور الخاصة بك",
+  "renter_password": "إعادة إدخال كلمة المرور",
+  "renter_password_hint": "يرجى إعادة إدخال كلمة المرور الخاصة بك",
+  "phone_number": "رقم الهاتف",
+  "phone_number_hint": "أدخل رقم هاتفك",
+  "country": "الدولة",
+  "country_hint": "أدخل دولتك",
+  "remember_me": "تذكرني",
+  "dont_have_an_account": "ليس لديك حساب؟",
+  "already_have_an_account": "هل لديك حساب بالفعل؟",
+  "or_login_with": "أو تسجيل الدخول باستخدام",
+  "reset_password": "إعادة تعيين كلمة المرور",
+  "reset_by_email": "إعادة تعيين عبر البريد الإلكتروني",
+  "reset_by_phone": "إعادة تعيين عبر رقم الهاتف",
+  "reset_password_method": "اختر كيفية إعادة تعيين كلمة المرور",
+  "reset_password_email_sub_title": "أدخل عنوان البريد الإلكتروني الذي استخدمته عند الانضمام وسنرسل لك تعليمات لإعادة تعيين كلمة المرور",
+  "reset_password_phone_sub_title": "أدخل رقم الهاتف الذي استخدمته عند الانضمام وسنرسل لك تعليمات لإعادة تعيين كلمة المرور",
+  "remembered_your_password": "هل تتذكر كلمة المرور الخاصة بك؟",
+  "check_your_email": "تحقق من بريدك الإلكتروني",
+  "check_your_email_sub_title": "لقد أرسلنا بريدًا إلكترونيًا إلى عنوان بريدك الإلكتروني",
+  "next": "التالي",
+  "welcome": "مرحباً",
+  "welcome_sub_title": "انظر إلى أحدث المنتجات والاكسسوارات",
+  "name": "الاسم",
+  "name_hint": "يرجى إدخال اسمك",
+  "verification": "التحقق",
+  "verification_sub_text": "يرجى إدخال الرمز المرسل إلى الرقم",
+  "check_email": "تحقق من بريدك الإلكتروني",
+  "check_email_subtitle": "لقد أرسلنا كلمة مرور إعادة تعيين إلى عنوان بريدك الإلكتروني",
+  "didnt_receive_code": "لم تتلقى الرمز؟",
+  "resend": "إعادة إرسال",
+  "done": "تم",
+  "all_set": "كل شيء جاهز!",
+  "your_account_active": "تم تنشيط حسابك",
+  "chose_a_method": "من فضلك اختر الطريقه التي تريد التود الدخول بها للتطبيق",
+  "register_by_google": "تسجيل الدخول عن طريق google",
+  "register_by_facebook": "تسجيل الدخول عن طريق facebook"
+};
+static const Map<String,dynamic> en = {
+  "login": "Login",
+  "login_sub_title": "Please login to explore our products",
+  "register": "Register",
+  "register_sub_title": "register to explore our products and latest news",
+  "username": "Username",
+  "username_hint": "please enter your username",
+  "email": "Email",
+  "email_hint": "please enter your Email",
+  "password": "Password",
+  "password_hint": "please enter your password",
+  "renter_password": "Renter Password",
+  "renter_password_hint": "please renter your password",
+  "phone_number": "Phone number",
+  "phone_number_hint": "Enter your phone number",
+  "country": "Phone number",
+  "country_hint": "Enter your phone number",
+  "remember_me": "Remember me",
+  "dont_have_an_account": "Don't have an account?",
+  "already_have_an_account": "already have an account?",
+  "or_login_with": "Or Login With",
+  "reset_password": "Reset Password",
+  "reset_by_email": "Reset by email",
+  "reset_by_phone": "Reset by phone number",
+  "reset_password_method": "select how to want to reset your password",
+  "reset_password_email_sub_title": "Enter the email address you used when you joined and we'll send you instructions to reset your password",
+  "reset_password_phone_sub_title": "Enter the phone number you used when you joined and we'll send you instructions to reset your password",
+  "remembered_your_password": "You Remember your password?",
+  "check_your_email": "Check your Email",
+  "check_your_email_sub_title": "We have sent an email to your email address",
+  "next": "Next",
+  "welcome": "welcome",
+  "welcome_sub_title": "see the latest products and accessories",
+  "name": "Name",
+  "name_hint": "please enter your name",
+  "verification": "Verification",
+  "verification_sub_text": "please enter the code sent to the number",
+  "check_email": "Check your Email",
+  "check_email_subtitle": "We have sent a reset password to your email address",
+  "didnt_receive_code": "Didn't receive code?",
+  "resend": "Resend",
+  "done": "Done",
+  "all_set": "All Set!",
+  "your_account_active": "Your account have been activated",
+  "chose_a_method": "Please chose a method to login to the app",
+  "register_by_google": "Register by google",
+  "register_by_facebook": "Register by facebook"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar": ar, "en": en};
+}
---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,51 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const login = 'login';
+  static const login_sub_title = 'login_sub_title';
+  static const register = 'register';
+  static const register_sub_title = 'register_sub_title';
+  static const username = 'username';
+  static const username_hint = 'username_hint';
+  static const email = 'email';
+  static const email_hint = 'email_hint';
+  static const password = 'password';
+  static const password_hint = 'password_hint';
+  static const renter_password = 'renter_password';
+  static const renter_password_hint = 'renter_password_hint';
+  static const phone_number = 'phone_number';
+  static const phone_number_hint = 'phone_number_hint';
+  static const country = 'country';
+  static const country_hint = 'country_hint';
+  static const remember_me = 'remember_me';
+  static const dont_have_an_account = 'dont_have_an_account';
+  static const already_have_an_account = 'already_have_an_account';
+  static const or_login_with = 'or_login_with';
+  static const reset_password = 'reset_password';
+  static const reset_by_email = 'reset_by_email';
+  static const reset_by_phone = 'reset_by_phone';
+  static const reset_password_method = 'reset_password_method';
+  static const reset_password_email_sub_title = 'reset_password_email_sub_title';
+  static const reset_password_phone_sub_title = 'reset_password_phone_sub_title';
+  static const remembered_your_password = 'remembered_your_password';
+  static const check_your_email = 'check_your_email';
+  static const check_your_email_sub_title = 'check_your_email_sub_title';
+  static const next = 'next';
+  static const welcome = 'welcome';
+  static const welcome_sub_title = 'welcome_sub_title';
+  static const name = 'name';
+  static const name_hint = 'name_hint';
+  static const verification = 'verification';
+  static const verification_sub_text = 'verification_sub_text';
+  static const check_email = 'check_email';
+  static const check_email_subtitle = 'check_email_subtitle';
+  static const didnt_receive_code = 'didnt_receive_code';
+  static const resend = 'resend';
+  static const done = 'done';
+  static const all_set = 'all_set';
+  static const your_account_active = 'your_account_active';
+  static const chose_a_method = 'chose_a_method';
+  static const register_by_google = 'register_by_google';
+  static const register_by_facebook = 'register_by_facebook';
+
+}
